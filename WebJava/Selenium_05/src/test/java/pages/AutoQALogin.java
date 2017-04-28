80187e10cfcf9569aad7622c092aa79217f56d66
Ilya Shebanov <ivshebanov@gmail.com>
2017-04-29 01:00:01 +0300

    Авто отправка писем POM

--- WebJava/Selenium_05/src/test/java/pages/AutoQALogin.java
+++ WebJava/Selenium_05/src/test/java/pages/AutoQALogin.java
@@ -2,39 +2,49 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 
 public class AutoQALogin {
-    WebDriver driver;
-    By userName = By.id("mailbox__login");
-    By password = By.id("mailbox__password");
-    By login = By.id("mailbox__auth__button");
+    private WebDriver driver;
+    private WebDriverWait wait;
+    private By userName = By.id("mailbox__login");
+    private By password = By.id("mailbox__password");
+    private By buttonlogin = By.id("mailbox__auth__button");
+    private By checkLogin = By.id("PH_user-email");
 
     public AutoQALogin(WebDriver driver) {
         this.driver = driver;
+        wait = new WebDriverWait(driver, 15);
     }
 
     private void setUserName(String strUserName) {
+        driver.findElement(userName).clear();
         driver.findElement(userName).sendKeys(strUserName);
     }
 
     private void setPassword(String strPassword) {
+        driver.findElement(password).clear();
         driver.findElement(password).sendKeys(strPassword);
     }
 
     private void clickLogin() {
-        driver.findElement(login).click();
+        WebElement button = wait.until(ExpectedConditions.visibilityOfElementLocated(buttonlogin));
+        button.click();
+        wait.until(ExpectedConditions.stalenessOf(button));
     }
 
-    /**
-     * This POM method will be exposed in test case to login in the application
-     *
-     * @param strUserName
-     * @param strPasword
-     * @return
-     */
+    private void isCheckLogin(String userName) {
+        String text = driver.findElement(checkLogin).getText();
+        Assert.assertEquals(userName, text);
+    }
+
     public void loginToAutoQA(String strUserName, String strPasword) {
         this.setUserName(strUserName);
         this.setPassword(strPasword);
         this.clickLogin();
+        this.isCheckLogin(strUserName);
     }
 }
--- WebJava/Selenium_05/src/test/java/test/TestAutoQALogin.java
+++ WebJava/Selenium_05/src/test/java/test/TestAutoQALogin.java
@@ -6,21 +6,28 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import pages.AutoQALogin;
+import pages.AutoSending;
 
 import java.util.concurrent.TimeUnit;
 
 public class TestAutoQALogin {
-    WebDriver driver;
-    AutoQALogin objLogin;
-    private String strUserName = "";
-    private String strPasword = "";
+    private WebDriver driver;
+    private AutoSending objSending;
+    private AutoQALogin objLogin;
+
+    private String userName = "bkmz1951@mail.ru";
+    private String pasword = "12345qwerty";
+
+    private String recipientEmail = "ivshebanov@gmail.com";
+    private String letterSubject = "Тема накатана";
 
     @BeforeTest
     public void setup() {
         System.setProperty("webdriver.gecko.driver", "/usr/local/Cellar/geckodriver/0.16.0/bin/geckodriver");
         driver = new FirefoxDriver();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.get("https://www.instagram.com/");
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("https://mail.ru/");
     }
 
     @AfterMethod
@@ -28,14 +35,12 @@ public class TestAutoQALogin {
         driver.quit();
     }
 
-    /**
-     * This test case will login in http://autoqa.pp.ua/wp-login.php
-     * Login to application
-     * Verify the home page using Dashboard message
-     */
     @Test(priority = 0)
     public void test_Home_Page_Appear_Correct() throws InterruptedException {
-//        objLogin = new AutoQALogin(driver);
-//        objLogin.loginToAutoQA(strUserName, strPasword);
+        objLogin = new AutoQALogin(driver);
+        objLogin.loginToAutoQA(userName, pasword);
+
+        objSending = new AutoSending(driver);
+        objSending.loginToAutoQA(recipientEmail, letterSubject);
     }
 }
