24ebac390b4f2714813191a8454be1fbc285bb7b
Ilya Shebanov <ivshebanov@gmail.com>
2017-05-01 17:03:58 +0300

    Подтягиваем почты из базы

--- WebJava/Selenium_05/src/test/java/test/TestAutoQALogin.java
+++ WebJava/Selenium_05/src/test/java/test/TestAutoQALogin.java
@@ -1,5 +1,6 @@
 package test;
 
+import DAO.DaoMyClientServet;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
@@ -9,7 +10,7 @@ import org.testng.annotations.Test;
 import pages.AutoQALogin;
 import pages.AutoSending;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 
 public class TestAutoQALogin {
@@ -17,40 +18,43 @@ public class TestAutoQALogin {
     private AutoSending objSending;
     private AutoQALogin objLogin;
 
-    private static LinkedList<String> email;
+    private ArrayList<String> email;
     private String userName = "bkmz1951@mail.ru";
     private String pasword = "12345qwerty";
 
     private String recipientEmail = "";
     private String letterSubject = "TT";
 
+    private static ArrayList<String> initList(){
+        DaoMyClientServet daoMyClientServet = new DaoMyClientServet();
+        return (ArrayList<String>) daoMyClientServet.getUsersEmail();
+    }
 
     @BeforeTest
-    private static void initList() {
-        email = new LinkedList<String>();
-        email.addFirst("ivshebanov@gmail.com");
-        email.addFirst("1111ivshebanov1@gmail.com");
-        email.addFirst("2222ivshebanov2@gmail.com");
-        email.addFirst("3333ivshebanov3@gmail.com");
+    private void setupGecko() {
+        new TestAutoQALogin();
+        System.setProperty("webdriver.gecko.driver", "/usr/local/Cellar/geckodriver/0.16.0/bin/geckodriver");
+        email=initList();
+        System.out.println(email);
+        System.out.println(email.size());
     }
 
     @BeforeMethod
     public void setup() {
-        System.setProperty("webdriver.gecko.driver", "/usr/local/Cellar/geckodriver/0.16.0/bin/geckodriver");
         driver = new FirefoxDriver();
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
         driver.manage().window().maximize();
         driver.get("https://mail.ru/");
-        this.recipientEmail = email.getLast();
+        this.recipientEmail = email.get(0);
     }
 
     @AfterMethod
     public void tearDown() throws Exception {
         driver.quit();
-        email.removeLast();
+        email.remove(0);
     }
 
-    @Test(invocationCount = 4, timeOut = 50000)
+    @Test(invocationCount = 10)
     public void test_Home_Page_Appear_Correct() throws InterruptedException {
         objLogin = new AutoQALogin(driver);
         objLogin.loginToAutoQA(userName, pasword);
