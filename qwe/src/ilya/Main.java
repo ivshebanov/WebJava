496ddcb1962c6cf180a4f7a8b1382ecfe5fb4b18
Ilya Shebanov <ivshebanov@gmail.com>
2017-05-28 22:01:43 +0300

    Chromos

---
+++ qwe/src/ilya/Main.java
@@ -0,0 +1,10 @@
+package ilya;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Calculating calculating = new Calculating();
+        calculating.printTabl();
+        calculating.start(10);
+    }
+}
